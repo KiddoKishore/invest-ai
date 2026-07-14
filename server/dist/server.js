acda7a66a0b926a67bf6769bd623a04c69105b4a
Kishore S <kishores@Kishores-MacBook-Air-2.local>
2026-07-14 21:46:23 +0530

    initialize invest-ai project structure

---
+++ server/dist/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.get("/health", (_, res) => {
+    res.json({
+        success: true,
+        message: "InvestAI API is running 🚀",
+    });
+});
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+//# sourceMappingURL=server.js.map
